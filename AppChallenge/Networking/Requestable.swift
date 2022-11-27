fc9cbdacfc7dac2b9ebea3b8a98b21a039da4b57
Adonis Rumbwere <adonis.rumbwere@gmail.com>
2022-11-27 16:14:32 +0200

    setting up coordinator patten

--- AppChallenge/Networking/Requestable.swift
+++ AppChallenge/Networking/Requestable.swift
@@ -5,4 +5,11 @@
 //  Created by Adonis Rumbwere on 27/11/2022.
 //
 
+import Combine
 import Foundation
+
+public protocol Requestable {
+    var requestTimeOut: Float { get }
+    
+    func request<T: Codable>(_ req: NetworkRequest) -> AnyPublisher<T, NetworkError>
+}
--- AppChallenge/Services/generalServiceable.swift
+++ AppChallenge/Services/generalServiceable.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+import Combine
+
+protocol generalServiceable {
+    func getCurrentWeather() -> AnyPublisher<Weather, NetworkError>
+    
+}
+
+class GeneralService: generalServiceable {
+    
+    private var networkRequest: Requestable
+    
+    // inject this for testability
+    init(networkRequest: Requestable) {
+        self.networkRequest = networkRequest
+    }
+    
+    func getCurrentWeather() -> AnyPublisher<Weather, NetworkError> {
+        let endpoint = GeneralEndpoints.getCurrentWeather
+        let request = endpoint.createRequest()
+        return self.networkRequest.request(request)
+        
+    }
+    
+}
