f57d412e6c038a0a66a3bacfb7faea9f786fcc91
Adonis Rumbwere <adonis.rumbwere@gmail.com>
2022-11-29 20:54:40 +0200

    added activity indicator

---
+++ AppChallenge/Extras/ActivityIndicator.swift
@@ -0,0 +1,36 @@
+//
+//  ActivityIndicator.swift
+//  AppChallenge
+//
+//  Created by Adonis Rumbwere on 29/11/2022.
+//
+
+import UIKit
+
+fileprivate var aView: UIView?
+
+extension UIViewController {
+    
+    func showSpinner() {
+        aView = UIView(frame: self.view.bounds)
+        aView?.backgroundColor = UIColor.init(red: 0.5, green: 0.5, blue: 0.5, alpha: 0.5)
+        
+        let ai = UIActivityIndicatorView(style: .large)
+        ai.center = aView!.center
+        ai.startAnimating()
+        aView?.addSubview(ai)
+        self.view.addSubview(aView!)
+        
+        Timer.scheduledTimer(withTimeInterval: 20.0, repeats: false) { (t) in
+            self.removeSpinner()
+        }
+    }
+    
+    
+    func removeSpinner() {
+        aView?.removeFromSuperview()
+        aView = nil
+    }
+    
+    
+}
