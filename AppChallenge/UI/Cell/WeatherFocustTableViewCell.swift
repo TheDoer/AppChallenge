bc3d46c6c83d722bb2e16b313062ca6fc8ef5fd0
Adonis Rumbwere <adonis.rumbwere@gmail.com>
2022-11-29 06:49:46 +0200

    seting up Focust Weeather cell

--- AppChallenge/UI/Cell/WeatherFocustTableViewCell.swift
+++ AppChallenge/UI/Cell/WeatherFocustTableViewCell.swift
@@ -8,16 +8,27 @@
 import UIKit
 
 class WeatherFocustTableViewCell: UITableViewCell {
+    static let identifier = String(describing: WeatherFocustTableViewCell.self)
+    
+    @IBOutlet weak var focustDateLabel: UILabel!
+    @IBOutlet weak var weatherIcon: UIImageView!
+    @IBOutlet weak var tempLabel: UILabel!
+    
+    func focustWeatherSetUp(list: List) {
+        focustDateLabel.text =  getDayForDate(Date(timeIntervalSinceNow: Double(list.dt ?? Int(0.0))))
+        print(focustDateLabel.text)
+        
+        tempLabel.text = "\(list.main!.temp)"
+        
+        
+    }
+    
+    
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+       
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
 
 }
