63897ae1d554871db762ba866ff24e03b86934ff
Adonis Rumbwere <adonis.rumbwere@gmail.com>
2022-11-29 06:49:00 +0200

    clean focust model

--- AppChallenge/Model/FocustWeather.swift
+++ AppChallenge/Model/FocustWeather.swift
@@ -6,3 +6,50 @@
 //
 
 import Foundation
+
+// MARK: - FocustWeather
+struct FocustWeather: Codable {
+    let list: [List]?
+    let city: City?
+}
+
+// MARK: - City
+struct City: Codable {
+    let id: Int?
+    let name: String?
+    let coord: Coord?
+    let country: String?
+    let population: Int?
+    let timezone: Int?
+    let sunrise: Int?
+    let sunset: Int?
+}
+
+// MARK: - Coord
+struct Coord: Codable {
+    let lat: Double?
+    let lon: Double?
+}
+
+// MARK: - List
+struct List: Codable {
+    let dt: Int?
+    let main: MainFocust?
+    let weather: [WeatherFocust]?
+}
+
+// MARK: - Main
+struct MainFocust: Codable {
+    let temp: Double
+    let feelsLike: Double?
+    let tempMin: Double?
+    let tempMax: Double?
+}
+
+// MARK: - Weather
+struct WeatherFocust: Codable {
+    let id: Int?
+    let main: String?
+    let weatherDescription: String?
+    let icon: String?
+}
